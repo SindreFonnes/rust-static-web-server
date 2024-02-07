110de2fd52536b29ca38118bd0a4c7d81dd22a68
Sindre Fonnes <sindre.fonnes@gmail.com>
2024-02-07 21:38:32 +0100

    Implemented error response when it errors out

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use axum::{
     extract::State,
-    http::{header, HeaderValue},
-    response::IntoResponse,
+    http::{header, HeaderValue, StatusCode},
     routing::get,
     Router,
 };
@@ -10,34 +9,38 @@ use tower_http::{
     set_header::SetResponseHeaderLayer,
 };
 
-use axum::response::{Json, ErrorResponse};
-use serde_json::{Value, json};
+use axum::response::Json;
+use serde_json::{json, Value};
 
-async fn config(state: State<String>) -> impl IntoResponse {
-    let env = state.as_str();
-    let config_file = std::fs::read_to_string(format!("./config/{env}.json"));
+async fn config(
+    State(Environment(env)): State<Environment>,
+) -> Result<Json<Value>, (StatusCode, &'static str)> {
+    let config_file = std::fs::read_to_string(format!("./config/{env}.json")).map_err(|error| {
+        println!("{error}");
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Unable to load config file.",
+        )
+    })?;
 
-    let result = match config_file {
-        Ok(config_file_as_string) => config_file_as_string,
-        Err(_) => "Internal Server Error".to_owned(),
-    };
-
-    let config = serde_json::from_str(&result);
-
-    let mut config: Value = match config {
-        Ok(config) => config,
-        Err(_) => Value::Null,
-    };
+    let mut config: Value = serde_json::from_str(&config_file).map_err(|error| {
+        println!("{error}");
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Unable to parse config file.",
+        )
+    })?;
 
     if config.is_null() {
-        return Json(json!({
-            "error": "Failed to parse config file"
-        }))
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Config should not be null.",
+        ));
     }
 
-	config["env"] = json!(env);
+    config["env"] = json!(env);
 
-	Json(config)
+    Ok(Json(config))
 }
 
 #[derive(Debug, Clone)]
@@ -50,7 +53,7 @@ async fn main() {
 
     let serve_dir = ServeDir::new("public").not_found_service(ServeFile::new("public/index.html"));
 
-	let app = Router::new()
+    let app = Router::new()
 		.nest_service(
 			"/",
 			serve_dir,
@@ -61,9 +64,9 @@ async fn main() {
 		))
 		.route("/config", get(config));
 
-	let app = app.with_state(environment.0);
+    let app = app.with_state(environment);
 
-	println!("Serving at http://{address}");
+    println!("Serving at http://{address}");
 
     let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
 
